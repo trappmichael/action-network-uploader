66fb60f0ba4c3fbbed789861bd3cb75665f2dd87
Michael <trapp.michael@gmail.com>
2024-05-20 17:46:28 -0500

    First Commit

---
+++ src/main/java/org/trappmichael/actionnetworkuploader/ActionNetworkUploaderApplication.java
@@ -0,0 +1,13 @@
+package org.trappmichael.actionnetworkuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionNetworkUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionNetworkUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/trappmichael/actionnetworkuploader/ActionNetworkUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trappmichael.actionnetworkuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionNetworkUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
